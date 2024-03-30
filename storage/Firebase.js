a55bd0ccc78a07136ff2e169d700564efc6bb1d1
Kleiton Metzger <kleitonmetzger31@gmail.com>
2024-03-30 03:49:57 +0000

    Add Firebase authentication methods to PasswordReset and gender utility function to distinguish the gender by numbers

--- storage/Firebase.js
+++ storage/Firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut,sendPasswordResetEmail,deleteUser} from 'firebase/auth';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -21,6 +21,8 @@ const db = getFirestore(app);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
-export { storage, db, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
+
+
+export { storage, db, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut ,sendPasswordResetEmail,deleteUser};
 
 
---
+++ utils/gender.js
@@ -0,0 +1,12 @@
+
+export const getUSerSex = (gender) => {
+    if (gender === "1") {
+        return 'Masculino';
+    }
+    else if (gender === "2") {
+        return 'Feminino';
+    }
+    else {
+        return 'Não informado';
+    }
+}
