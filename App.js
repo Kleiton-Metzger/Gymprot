8aee899242f5a7e67507b4a7bd41d6475c3f0f80
Kleiton Metzger <kleitonmetzger31@gmail.com>
2024-04-12 14:19:50 +0100

    StatusBar added

--- App.js
+++ App.js
@@ -3,20 +3,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { FirebaseContext } from './contexts/auth';
-import {PaperProvider} from 'react-native-paper';
+import { PaperProvider } from 'react-native-paper';
 
-import  { Profile, CameraScreen, HomeScreen, UploadScreen, Login, Register, MyVideos, PrivateScreen, PublicScreen, EditProfile,ForgotPassword} from './screens';
+import {
+  Profile,
+  CameraScreen,
+  HomeScreen,
+  UploadScreen,
+  Login,
+  Register,
+  MyVideos,
+  PrivateScreen,
+  PublicScreen,
+  EditProfile,
+  ForgotPassword,
+} from './screens';
+import { getTabIconName } from './utils';
+import { LogBox, StatusBar } from 'react-native';
+import { AuthProvider } from './Hooks/useAuth';
 
-import { getTabIconName }from './utils'
+LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered.']);
 
 export default function App() {
   const Stack = createStackNavigator();
   const Tab = createBottomTabNavigator();
-  
-  
-  const TabNavigator = () => {
 
+  const TabNavigator = () => {
     return (
       <Tab.Navigator
         screenOptions={({ route }) => ({
@@ -31,31 +43,47 @@ export default function App() {
           },
         })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home',headerShown:false }} />
-        <Tab.Screen name="Upload" component={UploadScreen} options={{ title: 'Upload',headerShown:false }} />
-        <Tab.Screen name="Camera" component={CameraScreen} options={{ title: 'Camera',headerShown:false }} />
-        <Tab.Screen name="MyVideos" component={MyVideos} options={{ title: 'My Videos' ,headerShown:false}} />
-        <Tab.Screen name="Profile" component={Profile} options={{ title: 'Profile',headerShown:false}} />
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home', headerShown: false }} />
+        <Tab.Screen name="Upload" component={UploadScreen} options={{ title: 'Upload', headerShown: false }} />
+        <Tab.Screen name="Camera" component={CameraScreen} options={{ title: 'Camera', headerShown: false }} />
+        <Tab.Screen name="MyVideos" component={MyVideos} options={{ title: 'My Videos', headerShown: false }} />
+        <Tab.Screen name="Profile" component={Profile} options={{ title: 'Profile', headerShown: false }} />
       </Tab.Navigator>
     );
-  }
+  };
 
   return (
-
     <PaperProvider>
-    <NavigationContainer>
-      <FirebaseContext.Provider value={{}}>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false, gestureEnabled:false }} />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false,gestureEnabled:false}} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
-        <Stack.Screen name="ProfileScreen" component={TabNavigator} options={{ headerShown: false,gestureEnabled: false  }} />
-        <Stack.Screen name="PublicScreen" component={PublicScreen} options={{ headerShown: false,gestureEnabled:false }} />
-        <Stack.Screen name="PrivateScreen" component={PrivateScreen} options={{ headerShown: false,gestureEnabled:false }} />
-        <Stack.Screen name="EditProfileScreen" component={EditProfile} options={{ headerShown: false,gestureEnabled:false }} />
-      </Stack.Navigator>
-      </FirebaseContext.Provider>
-    </NavigationContainer>
+      <NavigationContainer>
+        <AuthProvider>
+          <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false, gestureEnabled: false }} />
+            <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
+            <Stack.Screen
+              name="ProfileScreen"
+              component={TabNavigator}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen
+              name="PublicScreen"
+              component={PublicScreen}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen
+              name="PrivateScreen"
+              component={PrivateScreen}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen
+              name="EditProfileScreen"
+              component={EditProfile}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+          </Stack.Navigator>
+        </AuthProvider>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
