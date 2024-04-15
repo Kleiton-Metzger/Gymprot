620ccc1301eb220638feafc35462f8f52ca36af7
Kleiton Metzger <kleitonmetzger31@gmail.com>
2024-04-15 05:44:27 +0100

    implementação de algoritmo para seguir users

--- components/common/seguirButton.js
+++ components/common/seguirButton.js
@@ -2,33 +2,45 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const SeguirBTN = ({ label, isFollowing, notFollowing, onPress }) => {
+  const buttonStyles = notFollowing ? styles.buttonNotFollowing : styles.buttonFollowing;
+  const textStyles = notFollowing ? styles.textNotFollowing : styles.textFollowing;
+
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.button, notFollowing ? { backgroundColor: '#581DB9' } : null]}>
-      <Text
-        style={[
-          styles.buttonText,
-          notFollowing ? { color: 'white', fontWeight: 'bold' } : { color: 'black', fontWeight: 'bold' },
-        ]}
-      >
-        {label}
-      </Text>
+    <TouchableOpacity onPress={onPress} style={buttonStyles}>
+      <Text style={textStyles}>{label}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  buttonNotFollowing: {
     width: 100,
     height: 40,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
     position: 'absolute',
-    backgroundColor: 'lightgrey',
+    backgroundColor: '#581DB9', // Cor do botão quando não estiver seguindo
   },
-  buttonText: {
+  buttonFollowing: {
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    position: 'absolute',
+    backgroundColor: 'lightgrey', // Cor do botão quando já estiver seguindo
+  },
+  textNotFollowing: {
     fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  textFollowing: {
+    fontSize: 16,
+    color: 'black', // Cor do texto quando já estiver seguindo
+    fontWeight: 'bold',
   },
 });
 
-export { SeguirBTN };
+export { SeguirBTN }; // Corrigindo a exportação aqui
--- screens/Profile/Follow/FolowerProfile/FolowerProfile.js
+++ screens/Profile/Follow/FolowerProfile/FolowerProfile.js
@@ -1,35 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { FontAwesome5, Feather } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './style';
-import { SeguirBTN } from '../../../../components';
-import { Video } from 'expo-av';
+import { SeguirBTN } from '../../../../components/common/seguirButton';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
+import { db } from '../../../../storage/Firebase';
+import { useAuth } from '../../../../Hooks/useAuth';
 
 export const FolowerProfile = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { userName, creatorAvatar, userBio } = route.params;
-  const [videos, setVideos] = useState([]);
+  const { currentUser } = useAuth();
+  const { userName, creatorAvatar, userBio, createBy } = route.params;
+  const [userData, setUserData] = useState(null);
   const [isFollowing, setIsFollowing] = useState(false);
   const [notFollowing, setNotFollowing] = useState(true);
 
-  const handleback = () => {
-    navigation.goBack();
-  };
+  useEffect(() => {
+    const fetchUserData = async () => {
+      // Função para buscar os dados do usuário da coleção 'users'
+      try {
+        const userDocRef = doc(db, 'users', createBy);
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          setUserData(userData);
+          setIsFollowing(
+            userData.seguidores && userData.seguidores.some(seguidor => seguidor.userId === currentUser?.userId),
+          );
+          setNotFollowing(
+            !userData.seguidores || !userData.seguidores.some(seguidor => seguidor.userId === currentUser?.userId),
+          );
+        } else {
+          setIsFollowing(false);
+          setNotFollowing(true);
+        }
+      } catch (error) {
+        console.error('Error fetching user data: ', error);
+      }
+    };
 
-  const handleSeguir = () => {
-    console.log('Seguir');
-    setIsFollowing(!isFollowing);
-    setNotFollowing(!notFollowing);
+    fetchUserData();
+  }, []);
+
+  const handleSeguir = async () => {
+    try {
+      const userDocRef = doc(db, 'users', createBy);
+      const currentUserDocRef = doc(db, 'users', currentUser.userId); // Documento do user atual
+
+      if (isFollowing) {
+        await updateDoc(userDocRef, {
+          seguidores: arrayRemove({
+            userId: currentUser.userId,
+            name: currentUser.name,
+            avatar: currentUser.avatar,
+          }),
+        });
+
+        await updateDoc(currentUserDocRef, {
+          seguindo: arrayRemove({
+            userId: createBy,
+            name: userName,
+            avatar: creatorAvatar,
+          }),
+        });
+
+        setIsFollowing(false);
+        setNotFollowing(true);
+      } else {
+        await updateDoc(userDocRef, {
+          seguidores: arrayUnion({
+            userId: currentUser.userId,
+            name: currentUser.name,
+            avatar: currentUser.avatar,
+          }),
+        });
+
+        await updateDoc(currentUserDocRef, {
+          seguindo: arrayUnion({
+            userId: createBy,
+            name: userName,
+            avatar: creatorAvatar,
+          }),
+        });
+
+        setIsFollowing(true);
+        setNotFollowing(false);
+      }
+    } catch (error) {
+      console.error('Error following user: ', error);
+    }
   };
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity style={styles.bckButton} onPress={handleback}>
+          <TouchableOpacity style={styles.bckButton} onPress={() => navigation.goBack()}>
             <Feather name="arrow-left" size={30} color="black" />
           </TouchableOpacity>
           <Text style={styles.title}>Perfil</Text>
@@ -39,20 +108,19 @@ export const FolowerProfile = () => {
           <Text style={styles.userName}>{userName}</Text>
           <View style={styles.userFollow}>
             <View style={styles.seguidoresContainer}>
-              <Text style={styles.segdrTxt}> Seguidores</Text>
-              <Text style={styles.segdrNum}> 10</Text>
+              <Text style={styles.segdrTxt}>Seguidor</Text>
+              <Text style={styles.segdrNum}>{userData?.seguidores?.length || 0} </Text>
             </View>
             <View style={styles.buttonContainer}>
               <SeguirBTN
                 label={isFollowing ? 'Seguindo' : 'Seguir'}
-                isFollowing={isFollowing}
-                notFollowing={notFollowing}
                 onPress={handleSeguir}
+                notFollowing={notFollowing}
               />
             </View>
             <View>
-              <Text style={styles.segdrTxt}> Seguindo</Text>
-              <Text style={styles.segdrNum}> 110</Text>
+              <Text style={styles.segdrTxt}>Seguindo</Text>
+              <Text style={styles.segdrNum}>{userData?.seguindo?.length || 0}</Text>
             </View>
           </View>
         </View>
@@ -61,6 +129,7 @@ export const FolowerProfile = () => {
         </View>
         <View style={styles.bodyContainer}>
           <Text style={styles.bodyTitle}>Body</Text>
+          <View>{/* Aqui você pode exibir os dados do usuário sendo seguido */}</View>
         </View>
       </View>
     </SafeAreaView>
