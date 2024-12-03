ff6346a247c0ed0efbc525dfe3f59e241fb3f612
Kleiton Metzger <76179933+Kleiton-Metzger@users.noreply.github.com>
2024-12-03 11:28:27 +0000

    Update Firebase.js

--- storage/Firebase.js
+++ storage/Firebase.js
@@ -13,12 +13,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAV1SMD9pfn1aWznBDnsn_AU2crTjN75DU',
-  authDomain: 'gymprot-f67c2.firebaseapp.com',
-  projectId: 'gymprot-f67c2',
-  storageBucket: 'gymprot-f67c2.appspot.com',
-  messagingSenderId: '502122880869',
-  appId: '1:502122880869:web:814514c8267fa257068b8c',
+ 
 };
 
 const app = initializeApp(firebaseConfig);
